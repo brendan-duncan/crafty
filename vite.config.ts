38b088822b1247aef5b9ae7217699f6ac56b99a3
Brendan Duncan <brendanduncan@gmail.com>
2026-05-12 13:28:01 -0600

    add samples to build

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,13 @@ export default defineConfig({
     emptyOutDir: true,
     rollupOptions: {
       input: {
-        main: 'crafty/index.html'
+        main: 'crafty/index.html',
+        directory: 'index.html',
+        cube_test: 'samples/cube_test.html',
+        cloud_test: 'samples/cloud_test.html',
+        cascade_shadow_test: 'samples/cascade_shadow_test.html',
+        forward_test: 'samples/forward_test.html',
+        procedural_test: 'samples/procedural_test.html',
       }
     }
   },
